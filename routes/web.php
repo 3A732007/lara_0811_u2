d065d23e508b426a47a7df9333a0db992e3d114f
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-10-23 00:20:48 +0800

    設定 Route 跳轉頁面

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,13 @@ use Illuminate\Support\Facades\Route;
 });
 */
 
-Route::get('/',function(){
+/*Route::get('/',function(){
     return 'welcome';
+});
+*/
+Route::get('r1', function(){
+    return redirect('r2');
+});
+Route::get('r2', function(){
+    return view('welcome');
 });
