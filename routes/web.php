3782069be5ef0275240eca99287bda5d8cd8aee0
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-10-23 00:22:33 +0800

    修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,13 @@ use Illuminate\Support\Facades\Route;
     return 'welcome';
 });
 */
-Route::get('r1', function(){
+/*Route::get('r1', function(){
     return redirect('r2');
 });
 Route::get('r2', function(){
     return view('welcome');
+});
+*/
+Route::get('hello/{name}',function($name){
+    return 'Hello,'.$name;
 });
