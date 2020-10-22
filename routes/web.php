7361bddd9c723056dc7ed471c8943605bf3ed348
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-10-23 00:24:08 +0800

    修改 Route 參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+/*
+ Route::get('/', function () {
     return view('welcome');
 });
 */
 
-/*Route::get('/',function(){
+/*
+ Route::get('/',function(){
     return 'welcome';
 });
 */
-/*Route::get('r1', function(){
+/*
+ Route::get('r1', function(){
     return redirect('r2');
 });
-Route::get('r2', function(){
+ Route::get('r2', function(){
     return view('welcome');
 });
 */
-Route::get('hello/{name}',function($name){
+/*
+ Route::get('hello/{name}',function($name){
     return 'Hello,'.$name;
+});
+*/
+Route::get('hello/{name?}',function($name='Everybody'){
+    return'Hello,'.$name;
 });
